069ee555a807d12c5593638047255aa0f8162e81
solitario-hn <elsolitariolafe@gmail.com>
2026-07-14 20:53:55 +0530

    first time explaination good

--- question.cpp
+++ question.cpp
@@ -83,27 +83,130 @@
 // }
 
 
-#include <iostream>
+// #include <iostream>
+// #include <vector>
+// #include <climits>
+// using namespace std;
+
+
+// int main(){
+//     vector<int> arr={0,1 ,1 ,0 ,2, 3 ,4 ,5 ,101 ,102 ,103 ,104 ,105 ,106 ,107, 108};
+//     int count=1; int longest=1;
+//     int smaller=INT_MIN;
+
+//     for(int i=0;i<arr.size();i++){
+//         if(arr[i]-1==smaller){
+//             count++;
+//             smaller=arr[i];
+//         }
+//         if(arr[i]!=smaller){
+//             count=1;
+//             smaller=arr[i];
+//         }
+//         longest=max(longest,count);;
+//     }
+//     cout<<longest;
+// }
+
+
+// 
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main(){
+//     int m=3;int n=3;
+//     vector<vector<int>> mat={{1,0,0},{0,0,0},{0,0,0}};
+//     vector<int> row(m, 0);
+//     vector<int> col(n, 0);
+
+//     // row = { 0 , 1 , 1}
+//     // col = { 0 , 1 , 1}
+
+
+//     for(int i=0;i<m;i++){
+//         for(int j=0;j<n;j++){
+//             if(mat[i][j] == 0 ) {
+//                 row[i] = 1;
+//                 col[j] = 1;
+//             }
+            
+//         }
+//     }
+
+//       for(int i=0;i<m;i++){
+//         for(int j=0;j<n;j++){
+//          if(row[i] == 1 || col[j] ==1) {
+//             mat[i][j] = 0;
+//          }
+            
+//         }
+//     }
+// }
+
 #include <vector>
-#include <climits>
+#include <iostream>
 using namespace std;
 
-
 int main(){
-    vector<int> arr={0,1 ,1 ,0 ,2, 3 ,4 ,5 ,101 ,102 ,103 ,104 ,105 ,106 ,107, 108};
-    int count=1; int longest=1;
-    int smaller=INT_MIN;
+    int m=3;int n=3;
+    vector<vector<int>> mat={{1,0,1},{1,1,1},{0,1,1}};
 
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]-1==smaller){
-            count++;
-            smaller=arr[i];
-        }
-        if(arr[i]!=smaller){
-            count=1;
-            smaller=arr[i];
-        }
-        longest=max(longest,count);;
+  bool firstROW=false; bool firstCOL=false;
+
+  //check row
+  for(int i=0;i<m;i++){
+    if(mat[0][i]==0){
+        firstROW=true;
     }
-    cout<<longest;
+  }
+
+  //check zeroth col
+  for(int i=0;i<n;i++){
+    if(mat[i][0]==0){
+        firstCOL=true;
+    }
+  }
+
+  //search for zero for remaining matrix and set 0 in first col and row at same position
+  for(int i=1;i<m;i++){
+    for(int j=1;j<n;j++){
+        if(mat[i][j]==0){
+            mat[i][0]=0;
+            mat[0][j]=0;
+        }
+    }   
+  }
+
+  //check for zero in firstrow and first col and make that respective element in rmaining matrix 0
+  for(int i=1;i<m;i++){
+    for(int j=1;j<n;j++){
+        if(mat[i][0]==0||mat[0][j] ==0)
+        mat[i][j]=0;      
+    }   
+  }
+
+  //now make firstrow zero
+  if(firstROW){
+    for(int i=0;i<m;i++){
+        mat[0][i]=0;
+    }
+  }
+
+  //now make first col zero
+    if(firstCOL){
+    for(int i=0;i<m;i++){
+        mat[i][0]=0;
+    }
+  }
+
+  for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        cout<<mat[i][j]<<',';
+    }
+    cout<<'\n';
+  }
 }
+// very good singed by rohit
